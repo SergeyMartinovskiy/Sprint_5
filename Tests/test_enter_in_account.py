f40d13e0d2c18b765979efb9d21852747520659b
Ser Martin <ser.martinovskiy@yandex.ru>
2025-02-15 11:19:26 +0300

    First

---
+++ Tests/test_enter_in_account.py
@@ -0,0 +1,8 @@
+from Locators import Locator
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from my_data import *
+
+class TestEnterLInLoginPage:
+    def test_login_click_on_button_personal_account_in_mane_page (self, general_settings):
+
