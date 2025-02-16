da380bd1294590711f5ba04e793e05c9aa3dfb5f
Ser Martin <ser.martinovskiy@yandex.ru>
2025-02-16 08:21:16 +0300

    First

--- Tests/test_transfers_in_constructor.py
+++ Tests/test_transfers_in_constructor.py
@@ -16,6 +16,7 @@ class TestTransfersInConstructorPage:
         assert checking_text_topping == check_choose_item
 
     def test_transfer_click_on_bread_button (self, general_settings):
+        general_settings.find_element(*Locator.button_topping).click()
         general_settings.find_element(*Locator.button_bread).click()
         checking_text_bread = general_settings.find_element(*Locator.text_bread_in_scroll).text
         check_choose_item = general_settings.find_element(*Locator.choose_section_of_item).text
